84cf4a177a6de87857e0c065f87bbc0a3b076c9b
yilin <sanny86827@gmail.com>
2021-07-05 13:38:23 +0800

    增加前後端程式碼註解

--- app2/api/query.php
+++ app2/api/query.php
@@ -5,6 +5,8 @@ include_once 'base.php';
 $classNum=$_GET['classNum'];
 $students=$Stu->all(" where substring(`class_num`,1,3)='$classNum'") ;
 //echo json_encode($students);
+
+//利用新陣列縮減要回傳給前端的json資料量
 $new=[];
 
 foreach ($students as $key => $value) {
@@ -17,7 +19,9 @@ foreach ($students as $key => $value) {
 }
 echo json_encode($new);
 
-/* foreach ($students as $key => $value) {
+/* 
+//在後端使用PHP語法先建立完整的HTML字串,再回傳給前端使用
+foreach ($students as $key => $value) {
     echo "<tr>";
     echo "<td class='id-num'>{$value['id']}</td>";
     echo "<td>{$value['nat_id']}</td>";
